5b8f17c6f1a53cdc4345329e6a5f39e1bdb9071e
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2017-11-21 14:34:58 +0100

    Implement diamond as singularity container

--- build.sh
+++ build.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 #This is required to build the stuff
 
+TOOL_VERSION="v0.9.13"
+
+# For download and temporary stuff
+TMPDIR=/build
+
+# Install directory
+INSTALLDIR=/usr/local/lib
+# Symbolic link location to the binaries
+BINDIR=/usr/local/bin
+
+mkdir $TMPDIR
+cd $TMPDIR
+
 set -euo pipefail
 
 #Add here what kind of dependencies you need in general
 #Check for packages here: https://pkgs.alpinelinux.org/packages
-apk --update add bash wget curl build-base gcc git python-dev ca-certificates py-pip freetype-dev libpng libpng-dev
+apk --update add bash wget gzip build-base gcc git ca-certificates
 
-#Run the installation here or in the Singularity file itself, no matter which one 
+#Run the installation here or in the Singularity file itself, no matter which one
+wget -P $TMPDIR "https://github.com/bbuchfink/diamond/releases/download/${TOOL_VERSION}/diamond-linux64.tar.gz"
+tar -xzf diamond-linux64.tar.gz --directory $INSTALLDIR
 
-#Create mountpoints that we need
-mkdir -p /lustre_cfc/qbic/megSAP-data
+ln -s $INSTALLDIR/diamond $BINDIR/diamond
+
+chmod +x $BINDIR/diamond
 
 #Clean up
 rm -rf /var/cache/apk/*
