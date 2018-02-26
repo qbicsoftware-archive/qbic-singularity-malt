9088bfc4c9d7c419e78d0da68851c52caaa81235
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-02-26 20:32:11 +0100

    Refactor container build

--- build.sh
+++ build.sh
@@ -6,37 +6,25 @@ TOOL_VERSION="0_4_0"
 # For download and temporary stuff
 TMPDIR=/build
 
-# Install directory
-INSTALLDIR=/usr/local/lib
-# Symbolic link location to the binaries
-BINDIR=/usr/local/bin
-
 FILE="MALT_unix_${TOOL_VERSION}.sh"
 
-
 mkdir $TMPDIR
-mkdir -p "/opt/java"
+
 cd $TMPDIR
 
-set -euo pipefail
+# Add here what kind of dependencies you need in general
+apt update && apt upgrade -y
+apt install -y wget gcc ca-certificates openjdk-8-jre
 
-#Add here what kind of dependencies you need in general
-#Check for packages here: https://pkgs.alpinelinux.org/packages
-apk --update add bash curl wget build-base gcc ca-certificates
-
-#Run the installation here or in the Singularity file itself, no matter which one
+# Run the installation here or in the Singularity file itself, no matter which one
 wget -P $TMPDIR "http://ab.inf.uni-tuebingen.de/data/software/malt/download/${FILE}"
 
-cp $FILE $INSTALLDIR
-
-chmod +x $INSTALLDIR/$FILE
-
-#ln -s $INSTALLDIR/diamond $BINDIR/diamond
-
-#chmod +x $BINDIR/diamond
+chmod +x $FILE
+# Run quiet installation
+./$FILE -q
 
 #Clean up
-rm -rf /var/cache/apk/*
+apt clean
 
 cd /
-rm -rf /build
+rm -rf $TMPDIR
